844d09c8df6ccd3dd2a09f7adc561e9c126daa60
Smartboygamer <113809876+Smartboygamer@users.noreply.github.com>
2025-11-08 20:41:30 +0600

    Update app.py

--- app.py
+++ app.py
@@ -99,5 +99,5 @@ def add_achievement():
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
