59a0ceca1c211c0bcf2ff4ad5ca295656ad7c490
Smartboygamer <113809876+Smartboygamer@users.noreply.github.com>
2025-11-08 20:38:47 +0600

    Update app.py

--- app.py
+++ app.py
@@ -96,5 +96,8 @@ def add_achievement():
     conn.close()
     return jsonify({'success': True})
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
